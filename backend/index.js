9e47b5809057ebd40191e6a0f52258b326037538
ddeveza <ddeveza@ymail.com>
2022-09-30 16:08:30 +0800

    completed the order status

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,10 @@
+import cors from 'cors';
 import express from 'express';
 import orderRoutes from './routes/order.js';
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 app.use('/orders', orderRoutes);
 
 app.listen(8000, () => {
